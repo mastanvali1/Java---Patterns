5c6060de5645e782734cbac02118c9c85b085c26
mastanvali1 <mastanshaik1331@gmail.com>
2025-07-30 18:57:57 +0530

    Complex patterns

---
+++ Complex_patterns/pattern_2.java
@@ -0,0 +1,51 @@
+class pattern_2{
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 4; j>=i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j<=i; j++) {
+                if(j==1 || j == i){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= 4; i++ ){
+            for (int j = 1 ; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 4; j>=i; j--) {
+                if( j == 4 || j == i){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+
+
+output:
+
+    * 
+   * * 
+  *   * 
+ *     * 
+*       * 
+ *     * 
+  *   * 
+   * * 
+    * 
+
+*/
